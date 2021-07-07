e3da3a19f57d1291877e1262fbee516c74b9ac6f
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-07 15:18:40 -0300

    initial commit

---
+++ src/main/java/com/example/otpvalidation/OtpvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.otpvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpvalidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/otpvalidation/OtpvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otpvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpvalidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
