e4da1e297f2de81d64f45e5eae8013ee2afce809
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-07 15:20:11 -0300

    created data model

---
+++ src/main/java/com/example/otpvalidation/model/PaymentConfirmation.java
@@ -0,0 +1,14 @@
+package com.example.otpvalidation.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class PaymentConfirmation {
+    @JsonProperty("TransactionID")
+    private String transactionID;
+    @JsonProperty("CreatedTime")
+    private Long createdTime;
+    @JsonProperty("OTP")
+    private String OTP;
+}
